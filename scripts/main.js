39db20040bda78666d1843fde76f7492ff690380
Patrice-H <webmestre64@gmail.com>
2022-10-25 00:03:35 +0200

    Add a dice on the scene

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'orbitControls';
+import { GLTFLoader } from 'gltfLoader';
 
 const scene = new THREE.Scene();
 
@@ -36,9 +37,24 @@ const material = new THREE.MeshPhongMaterial({
 const ground = new THREE.Mesh(geometry, material);
 ground.scale.set(50, 0.1, 50);
 ground.position.set(0, -0.05, 0);
-
 ground.name = 'ground';
 scene.add(ground);
+
+const loader = new GLTFLoader();
+loader.load(
+  'assets/dice.gltf',
+  (gltf) => {
+    const dice1 = gltf.scene.children[0];
+    dice1.scale.set(2, 2, 2);
+    dice1.position.set(0, 2, 0);
+    dice1.name = 'dice-1';
+    scene.add(dice1);
+  },
+  undefined,
+  (error) => {
+    console.error(error);
+  }
+);
 
 const render = () => {
   renderer.render(scene, camera);
