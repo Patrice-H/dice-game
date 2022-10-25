9b31939152f0adf8bf5ee64486fc9d80ffb37cd5
Patrice-H <webmestre64@gmail.com>
2022-10-25 09:48:13 +0200

    Outsource imported mesh creation

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,22 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'gltfLoader';
 import {
   scene,
   camera,
   renderer,
   initGraphicsUniverse,
   createGraphicGround,
+  createGraphicImportedMesh,
 } from './graphicsUniverse.js';
 
-// Meshes
-createGraphicGround(scene);
+// Add meshes
+createGraphicGround();
+createGraphicImportedMesh();
 
-const loader = new GLTFLoader();
-loader.load(
-  'assets/dice.gltf',
-  (gltf) => {
-    const dice1 = gltf.scene.children[0];
-    dice1.scale.set(2, 2, 2);
-    dice1.position.set(0, 2, 0);
-    dice1.name = 'dice-1';
-    scene.add(dice1);
-  },
-  undefined,
-  (error) => {
-    console.error(error);
-  }
-);
-
+// Render scene
 const render = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
 
+// init
 initGraphicsUniverse();
 render();
