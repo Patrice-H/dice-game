420820168c706967ee7b53c6b36803bd4dc00e7b
Patrice-H <webmestre64@gmail.com>
2022-10-25 11:55:14 +0200

    Add physics

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,43 @@
+import Ammo from './ammo.js';
 import {
   scene,
   camera,
   renderer,
+  clock,
   initGraphicsUniverse,
-  createGraphicGround,
-  createGraphicImportedMesh,
 } from './graphicsUniverse.js';
+import {
+  initPhysicsUniverse,
+  updatePhysicsUniverse,
+} from './physicsUniverse.js';
+import { createGround, createImportedMesh } from './meshes.js';
 
-// Add meshes
-createGraphicGround();
-createGraphicImportedMesh();
+Ammo().then((Ammo) => {
+  let physicsUniverse;
+  let tmpTransformation;
+  let rigidBody_List = new Array();
 
-// Render scene
-const render = () => {
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
-};
+  // Render scene
+  const render = () => {
+    let deltaTime = clock.getDelta();
+    updatePhysicsUniverse(
+      physicsUniverse,
+      rigidBody_List,
+      tmpTransformation,
+      deltaTime
+    );
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
 
-// init
-initGraphicsUniverse();
-render();
+  // init
+  tmpTransformation = new Ammo.btTransform();
+  physicsUniverse = initPhysicsUniverse(Ammo);
+
+  // Add meshes
+  createGround(Ammo, physicsUniverse, rigidBody_List, scene);
+  createImportedMesh(Ammo, physicsUniverse, rigidBody_List, scene);
+
+  initGraphicsUniverse();
+  render();
+});
