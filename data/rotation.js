ff9d565e02b220ea60c72fdf1cf6a4922442bbce
Patrice-H <webmestre64@gmail.com>
2022-10-28 11:00:27 +0200

    Add data file to get results

---
+++ data/rotation.js
@@ -0,0 +1,127 @@
+export const diceRotation = [
+  {
+    x: -parseFloat(Math.PI.toFixed(2)),
+    z: -parseFloat(Math.PI.toFixed(2)),
+    display: 1,
+  },
+  {
+    x: -parseFloat(Math.PI.toFixed(2)),
+    z: -parseFloat((Math.PI / 2).toFixed(2)),
+    display: 3,
+  },
+  {
+    x: -parseFloat(Math.PI.toFixed(2)),
+    z: 0,
+    display: 6,
+  },
+  {
+    x: -parseFloat(Math.PI.toFixed(2)),
+    z: parseFloat((Math.PI / 2).toFixed(2)),
+    display: 4,
+  },
+  {
+    x: -parseFloat(Math.PI.toFixed(2)),
+    z: parseFloat(Math.PI.toFixed(2)),
+    display: 1,
+  },
+  {
+    x: -parseFloat((Math.PI / 2).toFixed(2)),
+    z: -parseFloat(Math.PI.toFixed(2)),
+    display: 5,
+  },
+  {
+    x: -parseFloat((Math.PI / 2).toFixed(2)),
+    z: -parseFloat((Math.PI / 2).toFixed(2)),
+    display: 3,
+  },
+  {
+    x: -parseFloat((Math.PI / 2).toFixed(2)),
+    z: 0,
+    display: 2,
+  },
+  {
+    x: -parseFloat((Math.PI / 2).toFixed(2)),
+    z: parseFloat((Math.PI / 2).toFixed(2)),
+    display: 4,
+  },
+  {
+    x: -parseFloat((Math.PI / 2).toFixed(2)),
+    z: parseFloat(Math.PI.toFixed(2)),
+    display: 5,
+  },
+  {
+    x: 0,
+    z: -parseFloat(Math.PI.toFixed(2)),
+    display: 6,
+  },
+  {
+    x: 0,
+    z: -parseFloat((Math.PI / 2).toFixed(2)),
+    display: 4,
+  },
+  {
+    x: 0,
+    z: 0,
+    display: 1,
+  },
+  {
+    x: 0,
+    z: parseFloat((Math.PI / 2).toFixed(2)),
+    display: 3,
+  },
+  {
+    x: 0,
+    z: parseFloat(Math.PI.toFixed(2)),
+    display: 6,
+  },
+  {
+    x: parseFloat((Math.PI / 2).toFixed(2)),
+    z: -parseFloat(Math.PI.toFixed(2)),
+    display: 2,
+  },
+  {
+    x: parseFloat((Math.PI / 2).toFixed(2)),
+    z: -parseFloat((Math.PI / 2).toFixed(2)),
+    display: 3,
+  },
+  {
+    x: parseFloat((Math.PI / 2).toFixed(2)),
+    z: 0,
+    display: 5,
+  },
+  {
+    x: parseFloat((Math.PI / 2).toFixed(2)),
+    z: parseFloat((Math.PI / 2).toFixed(2)),
+    display: 4,
+  },
+  {
+    x: parseFloat((Math.PI / 2).toFixed(2)),
+    z: parseFloat(Math.PI.toFixed(2)),
+    display: 2,
+  },
+  {
+    x: parseFloat(Math.PI.toFixed(2)),
+    z: -parseFloat(Math.PI.toFixed(2)),
+    display: 1,
+  },
+  {
+    x: parseFloat(Math.PI.toFixed(2)),
+    z: -parseFloat((Math.PI / 2).toFixed(2)),
+    display: 3,
+  },
+  {
+    x: parseFloat(Math.PI.toFixed(2)),
+    z: 0,
+    display: 6,
+  },
+  {
+    x: parseFloat(Math.PI.toFixed(2)),
+    z: parseFloat((Math.PI / 2).toFixed(2)),
+    display: 4,
+  },
+  {
+    x: parseFloat(Math.PI.toFixed(2)),
+    z: parseFloat(Math.PI.toFixed(2)),
+    display: 1,
+  },
+];
