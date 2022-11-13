05123afedbb34b2566647f446186f293be660365
Patrice-H <webmestre64@gmail.com>
2022-11-13 09:52:01 +0100

    Display pointer cursor on selected dice

--- scripts/mouseManager.js
+++ scripts/mouseManager.js
@@ -4,9 +4,17 @@ import { getSelectedObject } from './utilsfunctions.js';
 const mouse = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
 
-export const onMouseMove = (event) => {
+export const onMouseMove = (event, scene, camera) => {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -((event.clientY / window.innerHeight) * 2 - 1);
+  raycaster.setFromCamera(mouse, camera);
+  const objectsTouched = raycaster.intersectObjects(scene.children);
+  const selectedObject = getSelectedObject(objectsTouched);
+  if (selectedObject !== undefined) {
+    document.body.style = 'cursor: pointer;';
+  } else {
+    document.body.style = 'cursor: default;';
+  }
 };
 
 export const onMouseUp = (scene, camera) => {
@@ -14,6 +22,6 @@ export const onMouseUp = (scene, camera) => {
   const objectsTouched = raycaster.intersectObjects(scene.children);
   const selectedObject = getSelectedObject(objectsTouched);
   if (selectedObject !== undefined) {
-    console.log(selectedObject.name);
+    console.log(selectedObject);
   }
 };
